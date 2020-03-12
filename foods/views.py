ed175db1c0be91b2d9336b93196771f9216fdfe5
Brian Hobbs <brianhobbs216@gmail.com>
2020-03-12 16:54:21 -0400

    quick clean up

--- foods/views.py
+++ foods/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
 
+
 def index(request):
     item_list = Item.objects.all()
     
@@ -9,11 +10,12 @@ def index(request):
         'item_list': item_list,
     }
     
-    
     return render(request, 'index.html', context)
+
 
 def item(request):
     return render(request, 'item.html')
+
 
 def detail(request, item_id):
     item = Item.objects.get(pk=item_id)
